ecae99aad3ee912dfcc6d3e0fa5607911d7a3b62
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-04-04 12:59:49 -0500

    07 listo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream> // cout 
 #include "arit.h"   // suma, resta
 
-// g++ -std=c++2b main.cpp arit.cpp -o main
+// Antes: g++ -std=c++2b main.cpp arit.cpp -o main
+// Ahora                : make
+// Limpia los temporales: make clean
+// Compile todo.        : make -B
+// Luego: ./main
+
 // El main() debe ser muy pero muy pequeñito
 int main() {
-    std::cout << "Hello FundProg-UNI!" << std::endl;
+    std::cout << "HelloX FundProg-UNI!" << std::endl;
     DemoAritmetica();
     return 0;
 }
