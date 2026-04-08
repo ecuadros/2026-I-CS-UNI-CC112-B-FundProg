935b723727ac499d7c57e05886c8daef05660d0b
JoseMedina57 <josemedinacanduelas231@gmail.com>
2026-04-07 22:02:24 -0500

    código después de descomentar

--- sorting.h
+++ sorting.h
@@ -55,13 +55,110 @@ void BurbujaRecursivo(T arr[], ContainerRange n, Func func) {
 }
 void DemoBurbuja();
 
-// ContainerRange  particionar(ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);
-// void QuickSort  (ContainerElemType1* arr, ContainerRange first, ContainerRange last, CompFunc pComp);
-// void DemoQuickSort();
+// QuickSort
+template <typename T, typename Func>
+void QuickSort(T arr, ContainerRange first, ContainerRange last, Func pComp) {
+    if (first < last) {
+        auto pi = particionar(arr, first, last, pComp);
+        QuickSort(arr, first, pi - 1, pComp);
+        QuickSort(arr, pi + 1, last, pComp);
+    }
+}
 
-// void Merge(ContainerElemType1* arr, const ContainerRange left, const ContainerRange mid, const ContainerRange right, CompFunc pComp); 
-// void MergeSort(ContainerElemType1* arr, const ContainerRange begin, const ContainerRange end, CompFunc pComp);
-// void DemoMergeSort();
+//MergeSort
+template<typename T, typename S>
+void MergeSort( T arr[],
+                ContainerRange const begin,
+                ContainerRange const end,
+                S pComp) {
+    if (begin >= end)
+        return; // Return recursivamente
+
+    auto mid = begin + (end - begin) / 2;
+    MergeSort(arr, begin, mid, pComp);
+    MergeSort(arr, mid + 1, end, pComp);
+    MergeSort(arr, mid, end, pComp);
+}
+
+// // Función para mezclar dos subarreglos ordenados de arr[].
+template<typename T, typename Func>
+void Merge(T arr[], const ContainerRange left,
+                                   const ContainerRange mid,
+                                   const ContainerRange right,
+                                   Func pComp) {
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    // Crear arrays temporales
+    auto *leftArray = new T1[subArrayOne],
+         *rightArray = new T1[subArrayTwo];
+
+    // Copiar datos a los arrays temporales leftArray[] y rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = arr[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = arr[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, // Índice inicial del primer sub-array
+        indexOfSubArrayTwo = 0; // Índice inicial del segundo sub-array
+    ContainerRange indexOfMergedArray = left; // Índice inicial del array
+
+    //Mezclar los arrays temporales de vuelta a arr[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo <
+    subArrayTwo) {
+        if ( (*pComp)(rightArray[indexOfSubArrayTwo],
+        leftArray[indexOfSubArrayOne])  ) {
+            arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        } else {
+            arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copiar los elementos restantes de left[], si los hay
+    while (indexOfSubArrayOne < subArrayOne) {
+        arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copiar los elementos restantes de right[], si los hay
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+template<typename T, typename Func>
+ContainerRange particionar(T arr[], ContainerRange first, ContainerRange
+last, Func pComp) {
+    auto pivote = arr[last];  // Pivote es el elemento de referencia
+    auto i = (first - 1);
+
+    for (auto j = first; j <= last - 1; j++) {
+        if (arr[j] == pivote) ++i;
+        if ( (*pComp)(arr[j], pivote) ){
+            ++i; intercambiar(arr[i], arr[j]);
+        }
+    }
+    intercambiar(arr[i + 1], arr[last]);
+    return (i + 1);
+}
+
+template<typename T, typename S>
+ContainerRange  particionar(T arr[], ContainerRange first, ContainerRange last, S pComp);
+template<typename T, typename Func>
+void QuickSort (T arr[], ContainerRange first, ContainerRange last, Func pComp);
+void DemoQuickSort();
+template<typename T, typename S>
+void MergeSort(T arr[], ContainerRange const left, ContainerRange const mid, const ContainerRange right, S pComp);
+template<typename T, typename S>
+void MergeSort(T arr[], const ContainerRange begin, const ContainerRange end, S pComp);
+void DemoMergeSort();
 
 void DemoSorting();
 
