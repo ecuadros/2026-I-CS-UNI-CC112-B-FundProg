a19c9d9ba9c14e22addd0be1b7db6e35bda5f521
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2026-04-21 17:06:42 -0500

    ejercicio

--- matrix.cpp
+++ matrix.cpp
@@ -4,9 +4,40 @@
 
 using namespace std;
 
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols) {
+    pMat = new TP *[rows];
+    for (size_t i = 0; i < rows; ++i) 
+        pMat[i] = new TP [cols];
+}
+
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols) {
+    cout << "Ingrese los valores: " << endl;
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            cin >> pMat[i][j];
+        } 
+    }
+}
+
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            cout << pMat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DeleteMatrix(TP **&pMat, const size_t rows) {
+    for (size_t i = 0; i < rows; ++i) 
+        delete[] pMat[i];
+    delete[] pMat;
+    pMat = nullptr;
+}
+
 void DemoMatrix1(){
     size_t rows, cols;
-    // TP **pMat = nullptr;
+    TP **pMat = nullptr;
 
     cout << "Demostracion de matrizes" << endl;
     cout << "Ingrese nro de filas: ";
@@ -14,8 +45,9 @@ void DemoMatrix1(){
     cout << "Ingrese nro de columnas: ";
     cin >> cols;
     
-    // CreateMatrix(pMat, rows, cols);
-    // ReadMatrix(pMat, rows, cols);
-    // PrintMatrix(pMat, rows, cols);
-    // DeleteMatrix(pMat, rows);
+    CreateMatrix(pMat, rows, cols);
+    ReadMatrix(pMat, rows, cols);
+    PrintMatrix(pMat, rows, cols);
+    DeleteMatrix(pMat, rows);
 }
+
--- matrix.h
+++ matrix.h
@@ -1,8 +1,13 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
+#include <iostream>
 #include "types.h"
 
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols);
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols);
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols);
+void DeleteMatrix(TP **&pMat, const size_t rows);
 
 void DemoMatrix1();
 
