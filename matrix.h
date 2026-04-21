b91820356c531b7ead202bcfa1c2196da846af23
Rodrigo <turambaradanedhel@gmail.com>
2026-04-21 16:34:48 -0500

    MATRIX

--- matrix.h
+++ matrix.h
@@ -1,8 +1,13 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
+#include <iostream> 
 #include "types.h"
 
+void CreateMatrix(TP **&pm, const size_t rows, const size_t cols);
+void ReadMatrix  (TP **pm, const size_t rows, const size_t cols);
+void PrintMatrix (TP **pm, const size_t rows, const size_t cols);
+void DeleteMatrix(TP **&pm, const size_t rows);
 
 void DemoMatrix1();
 
