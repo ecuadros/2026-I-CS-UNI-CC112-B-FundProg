e59cd88f263e86cf7722fe4f88b5fd4282230afa
kevc227 <kevincalderonzarate@gmail.com>
2026-04-22 13:59:43 -0500

    tarea

--- matrix.cpp
+++ matrix.cpp
@@ -1,21 +1,46 @@
 #include "matrix.h"
 #include <iostream>
-
-
 using namespace std;
-
 void DemoMatrix1(){
     size_t rows, cols;
-    // TP **pMat = nullptr;
-
-    cout << "Demostracion de matrizes" << endl;
+    TP **pMat = nullptr; 
+    cout << "Demostracion de matrices" << endl;
     cout << "Ingrese nro de filas: ";
     cin >> rows;
     cout << "Ingrese nro de columnas: ";
     cin >> cols;
-    
-    // CreateMatrix(pMat, rows, cols);
-    // ReadMatrix(pMat, rows, cols);
-    // PrintMatrix(pMat, rows, cols);
-    // DeleteMatrix(pMat, rows);
+    CreateMatrix(pMat, rows, cols);
+    ReadMatrix(pMat, rows, cols);
+    PrintMatrix(pMat, rows, cols);
+    DeleteMatrix(pMat, rows);
+}
+void CreateMatrix(TP** &pMat, size_t filas, size_t columnas) {
+    pMat = new TP*[filas];
+    for (size_t i = 0; i < filas; i++) {
+        pMat[i] = new TP[columnas];
+    }
+}
+void ReadMatrix(TP** pMat, size_t filas, size_t columnas) {
+    for (size_t i = 0; i < filas; i++) {
+        for (size_t j = 0; j < columnas; j++) {
+            cout << "Matriz[" << i << "][" << j << "]: ";
+            cin >> pMat[i][j];
+        }
+    }
+}
+void PrintMatrix(TP** pMat, size_t filas, size_t columnas) {
+    for (size_t i = 0; i < filas; i++) {
+        for (size_t j = 0; j < columnas; j++) {
+            cout << pMat[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void DeleteMatrix(TP** &pMat, size_t filas) {
+    for (size_t i = 0; i < filas; i++) {
+        delete[] pMat[i];
+    }
+    delete[] pMat;
+    pMat = nullptr;
 }
